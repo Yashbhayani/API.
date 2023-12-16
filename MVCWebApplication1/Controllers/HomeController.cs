af8e78e9b43afcc85df5cf683c75bdad47ffab9e
Yash Bhayani <yashbhayani3007@gmail.com>
2023-12-16 10:26:31 +0530

    first commit

---
+++ MVCWebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MVCWebApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebApplication1.Controllers
+{
+    [Route("api/HomeController")]
+    public class HomeController : Controller
+    {        
+        [Route("index")]
+        [HttpGet]
+        public async Task<string> Test()
+        {
+            return "This is just demo";
+        }
+    }
+}
