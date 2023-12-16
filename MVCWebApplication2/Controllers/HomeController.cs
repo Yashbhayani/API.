a861dffbc0b106fef36f7c78062673c997ce67c3
Yash Bhayani <yashbhayani3007@gmail.com>
2023-12-16 10:28:20 +0530

    first commit

---
+++ MVCWebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using HttpGetAttribute = System.Web.Http.HttpGetAttribute;
+using RouteAttribute = System.Web.Http.RouteAttribute;
+
+namespace MVCWebApplication2.Controllers
+{
+    [Route("home")]
+    public class HomeController : ApiController
+    {
+        [Route("Yash")]
+        [HttpGet]
+        public IEnumerable Yash()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ MVCWebApplication2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebApplication2.WebApiApplication" Language="C#" %>
